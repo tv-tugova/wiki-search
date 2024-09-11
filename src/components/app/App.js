59dc891f8bf3214a15169ffdb4a087c25050023a
Tatiana <tvtugova1511@gmail.com>
2024-09-11 16:53:54 +0300

    Реализованы запрос к Wikipedia API и рендер заголовков статей

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,45 @@
+import React, { useEffect, useState } from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const apiBase = 'http://ru.wikipedia.org/w/api.php';
+    const query = 'Мастер и Маргарита';
+
+    const [titles, setTitles] = useState([]);
+
+    const request = async (url) => {
+        try {
+            let res = await fetch(url);
+
+            if (!res.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+            }
+
+            const data = await res.json();
+
+            return data[1];
+        } catch (error) {
+            console.error('Ошибка при выполнении запроса:', error);
+        }
+    }
+
+    const getAllArticles = async () => {
+        const result = await request(`${apiBase}?action=opensearch&search=${encodeURIComponent(query)}&limit=10&offset=0&format=json`);
+        setTitles(result);
+    }
+
+    useEffect(() => {
+        getAllArticles();
+    }, [])
+
+    return (
+        <div className="App">
+            <ul>
+                {titles.map((title, index) => (
+                    <li key={index}>{title}</li> 
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App;
